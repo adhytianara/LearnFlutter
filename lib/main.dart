9ef821feb4337abc29c87f2f2fcfee983a3d60a8
Adhytia <adit.14.12.99@gmail.com>
2021-01-17 21:59:24 +0700

    main screen with one clickable data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/detail_screen.dart';
+import 'package:learn_flutter/main_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Bandung',
       theme: ThemeData.dark(),
-      home: DetailScreen(),
+      home: MainScreen(),
     );
   }
 }
