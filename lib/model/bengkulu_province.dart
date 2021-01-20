ba54b7836b2e5940a5f37a359842a89eb1e71015
Adhytia <adit.14.12.99@gmail.com>
2021-01-20 10:49:24 +0700

    create bengkulu province model

---
+++ lib/model/bengkulu_province.dart
@@ -0,0 +1,19 @@
+class ProvinsiBengkulu {
+  String area = "- Total luas	19.788,70 km2\n- Lintang	5º 40' - 2º 0' LS\n- Longitude	100º 40' - 104º 0' BT";
+  String sejarah = "\t \t British East India Company (EIC) sejak 1685 mendirikan pusat perdagangan lada.  "
+      "Bencoolen/Coolen yang berasal dari bahasa inggris 'Cut Land' yang berarti tanah patah wilayah ini"
+      "  adalah wilayah patahan gempa bumi yang paling aktif di dunia dan kemudian gudang penyimpanan di"
+      "  tempat yang sekarang menjadi Kota Bengkulu.  \n"
+      "  \t \t Penemuan deposit emas di daerah Rejang Lebong pada"
+      "  paruh kedua abad ke-19 menjadikan tempat itu sebagai pusat penambangan emas hingga abad ke-20."
+      "  Saat ini, kegiatan penambangan komersial telah dihentikan semenjak habisnya deposit.  \n"
+      "  \t \t Pada tahun 1930-an,Bengkulu menjadi tempat pembuangan sejumlah aktivis pendukung kemerdekaan, termasuk Sukarno."
+      "  Pada masa inilah Sukarno berkenalan dengan Fatmawati yang kelak menjadi istrinya. Setelah kemerdekaan Indonesia,"
+      "  Bengkulu menjadi keresidenan dalam provinsi Sumatra Selatan. Baru sejak tanggal 18 November 1968"
+      "  ditingkatkan statusnya menjadi provinsi ke-26 (termuda sebelum Timor Timur).";
+  String populasi = " - Total	1.904.793\n- Kepadatan	96,25 jiwa/km2";
+
+  ProvinsiBengkulu();
+}
+
+ProvinsiBengkulu provinsiBengkulu = ProvinsiBengkulu();
